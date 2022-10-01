c2047a8f3727126ed54e6b7015d8b815f3c64b27
Issam <ighaouari@gmail.com>
2022-10-01 13:42:00 +0200

    zipProcedure

--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,10 @@
 function zipStrings(strA, strB) {
   // Add you solution here
+  let length = Math.min(strA.length , strB.length);
+  let ergebnis = "";
+  let i = 0;
+  for(i = 0; i < length; i++){
+    ergebnis += strA[i] + strB[i];
+  }
+  return ergebnis + strA.slice(i) + strB.slice(i);
 }
