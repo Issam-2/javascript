4c4a5efc016c362b0acc7fd8f9d885e664925a7a
Issam <ighaouari@gmail.com>
2022-10-01 12:46:21 +0200

    SumApp

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,23 @@
 class SumApp {
   // Add you solution here
+  constructor(){
+    this.numbers = [];
+  }
+  addNumber(n){
+    this.numbers.push(n);
+  }
+  getSum(){
+    if(this.numbers.length === 0){
+      return 0;
+    } else {
+      let sum = 0;
+      for(let i of this.numbers){
+        sum += i;
+      } return sum;
+    }
+  }
+  reset(){
+    this.numbers = [];
+  }
 }
+
